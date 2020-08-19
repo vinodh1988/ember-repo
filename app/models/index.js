275fa9397f65575fa963fd25588bef5f74b7f6e0
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-19 12:06:49 +0530

    ember data kickstart

---
+++ app/models/index.js
@@ -0,0 +1,8 @@
+import Model, {attr} from '@ember-data/model';
+
+export default class IndexModel extends Model {
+   
+  @attr title;
+  @attr itemlist;
+
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-    data = [
+  /*  data = [
         {title: "Industries" , itemlist: ["Aviation","Hospitality","Health Care", "Finance"]},
         {title: "Software Products", itemlist : ["XYZ ERP","ABD Developer Studio","Code Max","DTD DB"]},
         {title: "Technologies", itemlist :["Cloud Solution","Big Data Solution","Analytics"]}
-    ]
+    ] */
         
     
     async model() {
-          return this.data;
-    }
+          return this.store.findAll('index');
+
+          // it automatically search for api called indices
+          // the api url and namespace depends upon your adapter configuration
+  }
 }
