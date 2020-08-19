af2418165b463ecb50fdd6cbb9d795cdc67550f2
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-19 11:17:59 +0530

    Fetch Demo

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
+
 export default class ContactRoute extends Route {
+    async model(){
+        let response = await fetch("http://localhost:4800/api/offices");
+        let data = await response.json();
+        return data;
+        
+    }
 }
