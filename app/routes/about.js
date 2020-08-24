20610fd2e3207ed4f2b42cabf71410b26f4ae01e
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-24 11:30:37 +0530

    About JSON DATA

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default class AboutRoute extends Route {
+    model(){
+        return [
+            {type:"General", message ="This is a general message"},
+            {type:"General", message ="This is a general message 2"},
+            {type:"General", message ="This is a general message 3"},
+            {type:"Technical", message ="This is a Technical message"},
+            {type:"Technical", message ="This is a Technical message 2"},
+            {type:"Technical", message ="This is a Technical message 3"},
+            {type:"Finance", message ="This is a Finance message"},
+            {type:"Finance", message ="This is a Finance message 2"},
+            {type:"Finance", message ="This is a Finance message 3"},
+            {type:"Recruitment", message ="This is a Recruitment Message1"},
+            {type:"Recruitment", message ="This is a Recruitment Message2"}
+         ]
+    }
 }
