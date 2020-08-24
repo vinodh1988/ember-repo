94d0d6d07f853a0b24d3cfa77af0782d1d0fe69d
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-24 20:53:22 +0530

    Computed Property

--- app/components/about-details.js
+++ app/components/about-details.js
@@ -1,13 +1,64 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
+import { action,computed,set } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class AboutDetailsComponent extends Component {
-  @tracked firstpage = true;
-  @tracked secondpage = false;
-  @tracked thirdpage=false;
-  pageno = 1;
+    @tracked firstpage = true;
+    @tracked secondpage = false;
+    @tracked thirdpage=false;
+    @tracked question1="yes";
+    @tracked question2="yes";
+    pageno = 1;
+    info = [];
+    constructor(...args){
+      super(...args);
+      this.info = this.args.messages;
+      console.log(this.info)
+  }
 
+  @computed('info', 'question1','question2','secondpage','thirdpage')
+  get questionList() {
+    console.log("this is up and running")
+    if(this.secondpage){
+        let start;
+        if(this.question1=="yes")
+          start=0;
+        else
+          start=3;
+        let temp=[];
+        for(let i=start;i<6;i++)
+          temp.push(this.info[i])
+        return temp;
+    }
+
+    if(this.thirdpage){
+        let end;
+        if(this.question2=="yes")
+          end=11;
+        else
+          end=9;
+        let temp=[];
+        for(let i=6;i<end;i++)
+          temp.push(this.info[i])
+        return temp;
+    }
+    
+  }
+
+  @action
+  updateQuestion1(event){
+    this.question1 = event.target.value;
+    console.log(this.question1);
+  }
+
+  @action
+  updateQuestion2(event){
+
+    this.question2 = event.target.value;
+    console.log(this.question2);
+
+  }
+ 
   @action
   performForward(){
       this.pageno++;
@@ -37,3 +88,5 @@ export default class AboutDetailsComponent extends Component {
       }
   }
 }
+  
+
